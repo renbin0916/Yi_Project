bd0e793b6d3cec78f26e60c6396df8fbf6c2706d
易 <yi@leegame.net>
2019-12-28 14:50:09 +0800

    Update - initial

---
+++ Yi/Yi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yi
+//
+//  Created by 易 on 12/28/19.
+//  Copyright © 2019 易. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
